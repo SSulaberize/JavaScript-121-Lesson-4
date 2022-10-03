326e3a87ae24f960ef0395a93cf4d91566a58cca
SSulaberidze <ssulberidze@gmail.com>
2022-10-03 16:51:09 +0400

    To Do List

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-let navigationline = document.getElementById ('navigation-bar');
-let hamburgerbar = document.getElementById ('burger-bar');
-hamburgerbar.addEventListener ('click', function(){
-    hamburgerbar.classList.toggle('toggle');
-    navigationline.classList.toggle('active');
+// Burger Bar
+let navigationLine = document.getElementById ('navigation-bar');
+let hamburgerBar = document.getElementById ('burger-bar');
+hamburgerBar.addEventListener ('click', function(){
+    hamburgerBar.classList.toggle('toggle');
+    navigationLine.classList.toggle('active');
+})
+
+// To DO List
+let inputform = document.querySelector ('.product');
+let buttonadd = document.querySelector ('.button');
+let listofproducts = document.querySelector ('.products-list');
+let removeAllIcons = document.querySelector ('.clear');
+let form = document.getElementById ('form-wrapper');
+
+form.addEventListener ('submit', function(event) {
+    event.preventDefault();
+let value = inputform.value;
+let li = document.createElement ('li');
+let deleteIcon = document.createElement ("i");
+deleteIcon.innerHTML = '<i class="fa-solid fa-trash"></i>';
+deleteIcon.addEventListener ('click', function(){
+    li.remove();
+})
+li.textContent = value;
+li.appendChild(deleteIcon);
+listofproducts.appendChild(li);
+inputform.value = ' ';
+})
+
+removeAllIcons.addEventListener ('click', function(){
+    listofproducts.innerHTML = '';
 })
